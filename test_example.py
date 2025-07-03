a5c9ae0817d17db13c2ecb94708195a12ca3a66a
ella <elalek@gmail.com>
2025-07-03 20:28:35 +0200

    automaticke testy s ini

--- test_example.py
+++ test_example.py
@@ -11,7 +11,7 @@ def test_example():
 
                                                     # do terminalu napisat : pytest a enter
                                                     # funguje aj: python3 -m pytest
-
+@pytest.mark.positive
 def test_get_contact_positive():
     #zavolame funkci a jako parametr ji dame engeto 
      # dali sme na zaciatok, tam maju byt vsetky importy from contacts import get_contact
@@ -20,4 +20,22 @@ def test_get_contact_positive():
     assert result == {"name": "Engeto", "phone": "777777777"}
 
     # treba kazdy test pomenovat inak, pretoze zrovna na macbooku to zahlasi celkovu chybu. 
+@pytest.mark.negative
+def test_get_contact_negative():
+    result = get_contact("akademie")
+    assert result == None
 
+# def test_get_contact_negative_chybovy():
+#     result = get_contact("akademie")
+#     assert result == {"name": "Engeto", "phone": "777777777"}
+
+# roydelenie testov
+# 1. rychle
+# 2. zatezove
+
+# alebo pozitivni a negativni = @pytest.mark.positive - @ je dekorator
+
+# a vytvorim si novy dokument pytest.ini
+# a tam si dam tie znacky
+# a vdaka tomu mozem dat do terminalu poziadavku na spustenie len pozitivnych testov
+# pytest -m "positive"
