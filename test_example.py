f8a5de90eeeec82f92666b0dbae8f0e2cd394fcf
ella <elalek@gmail.com>
2025-07-03 21:02:46 +0200

    rozvinute

--- test_example.py
+++ test_example.py
@@ -2,6 +2,7 @@ import pytest
 from contacts import get_contact
 
 # testovací případ = funkci
+@pytest.mark.skip(reason="netreba")
 def test_example():
     # jednotlivé kroky testovacího případu
     result = 1 + 1
@@ -16,6 +17,15 @@ def test_get_contact_positive():
     #zavolame funkci a jako parametr ji dame engeto 
      # dali sme na zaciatok, tam maju byt vsetky importy from contacts import get_contact
     result = get_contact("Engeto")
+    #porovname vysledek
+    assert result == {"name": "Engeto", "phone": "777777777"}
+
+@pytest.mark.positive
+def test_get_contact_positive_2():
+    #zavolame funkci a jako parametr ji dame engeto 
+     # dali sme na zaciatok, tam maju byt vsetky importy from contacts import get_contact
+    result = get_contact("Engeto")
+    print(result)
     #porovname vysledek
     assert result == {"name": "Engeto", "phone": "777777777"}
 
@@ -39,3 +49,16 @@ def test_get_contact_negative():
 # a tam si dam tie znacky
 # a vdaka tomu mozem dat do terminalu poziadavku na spustenie len pozitivnych testov
 # pytest -m "positive"
+# alebo mozem skovat test @pytest.mark.skip(reason="je to zastarale")
+# aleboooo s podmienkkou @pytest.mark.skipif(......)
+# @pytest.mark.parametrise.......
+# este sa da pustit len jeden subor. v terminaly: pytest test_example.py
+# mozem pustit len 1 konkretny priklad a to v terminaly: pytest test_example.py::test_get_contact_negative
+# pytest default zachyti vsetky print funkcie a nezobrazi ich. iba ked chcem, tak do terminalu nakoniec napisem -s: pytest test_example.py::test_get_contact_positive_2 -s
+# pre porovnanie: pytest test_example.py::test_get_contact_positive_2
+# ak tam dame -v (v ako verbouse) zacne podrobne vypisovat co sa stalo v testoch: pytest -v
+# pri kazdom asserte mozem napisat informacnu hlasku 
+
+def test_get_contact_negative_chybovy():
+    result = get_contact("Engeto")
+    assert result == None, f"ocekavano None, ve skutecnosti {result}"
